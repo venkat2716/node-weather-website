8a86dc996808d75cfda2b51ebaa5d68c66ce2075
venkat2716 <vvari2716@gmail.com>
2020-04-10 19:41:05 +0530

    Help page updated

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text..',
+        helpText: 'How can we help you?',
         title: 'Help',
         name: 'Venkatesh'
     })
