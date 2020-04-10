9d0f01c4d18c79b1127f4d117102c2e243dd1c70
venkat2716 <vvari2716@gmail.com>
2020-04-10 22:35:56 +0530

    Remove global module

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value;
    
     messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+    messageTwo.textContent = '' 
 
     fetch('/weather?address='+ location).then((response) => {
         response.json().then((data) => {
